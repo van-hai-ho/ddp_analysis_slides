d79df53ad4434d674f687b22300fa28e5e4da704
unknown <van.hai.ho@gmail.com>
2014-08-22 14:09:12 +1000

    publishing deck

---
+++ deploy.R
@@ -0,0 +1,5 @@
+library(slidify)
+setwd("C:/Development/rstudio/shiny/ddp_analysis_slides")
+git config --global user.email "van.hai.ho@gmail.com"
+git config --global user.name "Van Hai Ho"
+publish(user = "vanho", repo = "ddp_analysis_slides")
